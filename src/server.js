edd70009d953cd5bef1c10f19ef49c579761e9b1
Sebastián Heras <herassebastian@gmail.com>
2022-05-16 12:30:13 -0300

    small change to server.js listening address

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = build({ logger: true });
 
 const start = async () => {
   try {
-    await app.listen(process.env.PORT || 3000);
+    await app.listen(process.env.PORT || 5000, "0.0.0.0");
   } catch (err) {
     app.log.error(err);
     process.exit(1);
